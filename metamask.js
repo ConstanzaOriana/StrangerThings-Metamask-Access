2e89e67b685b0e84dc5a132790f6221303e1e01c
Constanza Oriana <cotyoriana95@gmail.com>
2022-10-18 20:57:18 -0300

    first commit

--- metamask.js
+++ metamask.js
@@ -6,7 +6,16 @@ const accessToMetamask = () => {
         .then(result => {
             accountChangedHandler(result[0]);
         })} else {
-        alert("Install Metamask Pluggin");
+            Swal.fire({
+                title: 'What are you waiting for?',
+                color: 'red',
+                background: 'black',
+                text: 'Go install Metamask now!',
+                imageUrl: 'https://pa1.narvii.com/7957/dd0dec18fda53bcf3b7d5cbe43f6aa1cbf9daa8dr1-603-300_hq.gif',
+                imageWidth: 400,
+                imageHeight: 200,
+                imageAlt: 'Stranger things image',
+              });
     };
     const accountChangedHandler = (Account) => {
 		setDefaultAccount(Account);
